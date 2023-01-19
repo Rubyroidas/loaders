94e5ef82b11f70d2cf85427dca15dda84d2ba1ef
Denis Klyuev <denis.klyuev@gmail.com>
2023-01-18 23:14:13 -0500

    First loader

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+﻿console.debug('hello');
+const children = document.querySelectorAll('.loader-pulsing-circles > .layer');
+for (let i = 0; i < children.length; i++) {
+    (children[i] as HTMLElement).style.setProperty('--num', `${i}`);
+}
